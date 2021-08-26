c2aaea368c2be70a774410207d234a011794e766
Alfred Karanja <88835196+Akaranja1@users.noreply.github.com>
2021-08-26 17:31:22 +0300

    Add files via upload

---
+++ Seasons/src/Years.java
@@ -0,0 +1,74 @@
+public class Years {
+
+ public Integer YearRange;
+ public Integer RangeType;
+ public Integer FirstDivisor;
+ public Integer SecondDivisor;
+ public Integer FirstYear;
+ public Integer SecondYear;
+
+
+ public Years() {}
+
+
+ public Years(Integer divisibility) {
+
+   myFirstRange.setRangeType(1);
+  }
+
+ public Years(Integer olypmicyear) {
+
+   mySecondRange.setRangeType(2);
+  }
+
+ public Years(Integer leapyear) {
+   myThirdRange.setRangeType(3);
+  }
+  public void setRangeType (Integer Range){
+   return RangeType;
+
+  }
+ public  void YearRange(Integer Range){
+  return YearRange;
+ }
+public void getFirstDivisor( ){
+return FirstDivisor;
+}
+ public void getSecondDivisor(){
+return SecondDivisor;
+ }
+ public void getFirstYear(){
+  return FirstYear;
+ }
+ public void getSecondyear(){
+  return SecondYear;
+ }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
